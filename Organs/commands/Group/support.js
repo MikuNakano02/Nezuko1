adb6b2956b9ba1fb45e9b649fd9f7951147f037a
Miku <100752586+MikuNakano02@users.noreply.github.com>
2023-02-08 22:27:55 +0530

    Update support.js

--- Organs/commands/Group/support.js
+++ Organs/commands/Group/support.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: "support",
 	alias: ["sup"],
-	desc: "Send you official support group link.",
+	desc: "Links to support gc.",
 	category: "Group",
     react:"✅",
     start:async(client,m,{command,prefix,pushName})=>{
@@ -20,14 +20,14 @@ const eternitylogogg = "https://i.ibb.co/t3Jd1wK/Eternity-6.png"
 const lemo = `
 *━『 Support Group Links 』━*
 
-*1) [ Ɇ𝖙𝖊𝖗𝖓𝖎𝖙ɏ ] :*
-https://chat.whatsapp.com/JCCZPbPUbM1536n62zSFZi
+*1) [ ɴᴇᴢᴜᴋᴏ ] :*
+https://chat.whatsapp.com/JCCZPbPUbM1536n62sazSFZiada
 
-*2) [ Ɇ𝖙𝖊𝖗𝖓𝖎𝖙ɏ ₵𝖆𝖘𝖎𝖓ø ] 🎰 :*
-https://chat.whatsapp.com/J1WfFPXtmzZGXFB59I0ETq
+*2) [ ᴄᴀꜱɪɴᴏ ] 🎰 :*
+https://chat.whatsapp.com/J1WfFPXtmzZGasdasdXFB59I0ETq
 
-*3) [ Ɇ𝖙𝖊𝖗𝖓𝖎𝖙ɏ N̸s̸f̸w̸ ] :*
-https://chat.whatsapp.com/J1WfFPXtmzZGXFB59I0ETq
+*3) [ ɴꜱꜰᴡ] :*
+https://chat.whatsapp.com/J1WfFPXtmsdasadzZGXFB59I0ETq
 `
   await client.sendMessage(m.from,{video:{url:rae}, gifPlayback:true, caption: `*${pushName}* have a look in your DM`},{quoted:m})
   await  client.sendMessage(m.sender,{image:{url:eternitylogogg},caption:lemo},{quoted:m})  
