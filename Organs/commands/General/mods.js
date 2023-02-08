55796f973f7da1237bd2e30cf043e2905be9281c
Miku <100752586+MikuNakano02@users.noreply.github.com>
2023-02-08 22:20:25 +0530

    Update mods.js

--- Organs/commands/General/mods.js
+++ Organs/commands/General/mods.js
@@ -2,16 +2,16 @@
 require ('../../../settings')
 module.exports={
     name:"mods",
-    alias:["mod"],
+    alias:["mod,owner"],
     usage:`${prefa}mods`,
-    desc:"shows mods list",
+    desc:"shows owner",
     category:"General",
     react:"✅",
 
     start:async(client,m,{})=>{
         const mod= owner
         let mo=`
-*🎉 Nezuko Mods 🎉*\n`
+*🎉 ᴍʏ ᴏᴡɴᴇʀ 🎉*\n`
 for(let i=0;i<mod.length;i++){
     const um= await client.username(mod[i]+'@s.whatsapp.net')
     mo+=`\n✨${i+1}\n*👤 Name:* ${um}\n*📱 Contact:* http://wa.me/+${mod[i].split("@")[0]}\n`
