a9f2a3ba752330ec6b3b9dcaf384699acbf9e930
Miku <100752586+MikuNakano02@users.noreply.github.com>
2023-02-08 23:02:14 +0530

    Update boobjob.js

--- Organs/commands/Nsfw/boobjob.js
+++ Organs/commands/Nsfw/boobjob.js
@@ -21,7 +21,7 @@ module.exports={
     let buttonMessage = {
         image: {url:ass},
         caption: `*😈*`,
-        footer: `*©Eternity-Team*`,
+        footer: `*©ᴍɪɴᴀꜱᴇ*`,
         buttons: buttons,
         headerType: 4
     }
